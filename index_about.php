7a6bdcb998fa6f4b3a58f39dc34f72211ca49fcf
Paola-Cyp <73079863+Paola-Cyp@users.noreply.github.com>
2021-08-26 22:08:13 +0200

    Add files via upload

---
+++ index_about.php
@@ -0,0 +1,97 @@
+
+<!DOCTYPE html>
+<html>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="about.css" media="all" />
+  <body>
+
+    <nav class="navbar">
+          <ul class="links">
+              <li><a href="index.php">Home</a></li>
+              <li><a href="index_about.php">About</a></li>
+          </ul> 
+    </nav>
+
+    <div class="big_container">
+
+    <div class="page_container">
+
+
+        <div class="introduction">
+            <h1> À propos de notre projet </h1>
+            <p>
+                Nous avons un projet qui est de trouver des solutions qui à nos yeux nous paraissent faisables pour améliorer certaines conditions dans certains pays (voir la plupart) africains.
+                Nous avons tout d'abord repérer 2 principaux problèmes en Afrique :l'électricté et le PIB. 
+                645 millions, c’est le nombre d’Africains qui n’ont pas d’accès à une source d’électricité. 
+                En Afrique, les énergies renouvelables sont principalement utilisées pour produire de l’électricité, leur part dans les secteurs du transport et de production de chaleur étant encore faible.
+                En 2014, la moitié de la population du continent, qui s’élève à 1,2 milliard d’habitants, n’avait pas accès à l’électricité. Près de 730 millions de personnes utilisaient des méthodes de cuisson inefficaces et dangereuses, 
+                aux effets largement nocifs pour la santé des ménages, en particulier celle des femmes et des enfants.
+                Pour avoir une idée vous pouvez observer cette image ci-dessous 👇
+            </p>
+
+
+            <div class="center">
+              <img src='afrique_vue_du_ciel.jpeg' alt="image" height="870px" width="1100px">
+              <h2> Nos solutions :</h2>
+              <ul>
+                <li> Présence d'un acteur important (comme un opérateur national d’électricité)    </li>
+                <li> Mettre la technologie et la bonne gouvernance au service de l’électrification </li>
+                <li> Subventionner fortement le secteur </li>
+              </ul>
+            </div>
+            
+
+
+              
+
+          <div class="paragraphe_1">
+              <h3>Entreprendre </h3>
+              <p class="">
+              Les mini-réseaux constituent une solution viable pour améliorer l’accès à l’électricité dans les zones où l’extension du réseau coûte cher ou n’est pas encore possible.
+              Même si le développement de ce secteur est jusqu’ici limité en Afrique subsaharienne, à l’exception de la Tanzanie, plusieurs autres pays (comme le Nigéria et le Rwanda) ont entrepris d’importantes réformes de la réglementation afin de réduire les obstacles à ce type d’investissements.
+              Il y a deux principales raisons qui freinent l’engagement du secteur privé dans les mini-réseaux :le problème du recouvrement des coûts et l’incertitude quant aux perspectives d’avenir de cette infrastructure lorsque le réseau commencera à couvrir la zone en question.
+              « L’accès à l’électricité permettra des gains de productivité inter et intrasectorielle » , affirme Albert G. Zeufack, économiste en chef de la Banque mondiale pour la Région Afrique.  « Les pays africains doivent adopter la technologie et mettre à profit l’innovation pour que l’électricité soit de qualité, durable et bon marché. »
+              
+              </p>
+              
+          </div>
+          <br>
+          <div class="paragraphe_2">
+              <h3>Identifier notre sauveur </h3>
+              <p>L’expérience montre que la présence d’un acteur fort, comme un opérateur national d’électricité par exemple, est souvent décisive dans le succès des stratégies d’électrification. Forts de ce constat, de nombreux pays africains ont mis en place une agence d’électrification rurale dès la fin des années 1990. 
+              Malheureusement, faute de moyens et de compétences, ces agences n’ont pas toujours été les catalyseurs attendus.
+              Malgré ces échecs, identifier un chef de file capable de porter le développement de l’accès à l’électricité reste essentiel. Ce leader doit pouvoir fédérer les acteurs de l’accès et piloter des programmes d’ampleur en lien éventuel avec des co-financements de bailleurs internationaux. </p>
+              
+          </div>
+          <br>
+          <div class="paragraphe_3">
+                <h3>La subvention </h3>
+                <p>
+                  Déjà qu'est ce que la subvention🤔 ? 
+                  Une subvention économique est une subvention versée à des producteurs ou à des vendeurs de certains biens qui relèvent du secteur public ou privé, afin d'obtenir une baisse artificielle des prix de vente ou de revient.
+                  Atteindre le 100 % de foyers connectés nécessite de subventionner fortement le secteur, l’électrification rurale restant majoritairement déficitaire. Comme le constate l’étude, les foyers sans accès à l’électricité sont souvent établis dans des zones pauvres et reculées. Ils sont peu rentables à connecter car difficiles à desservir et faibles consommateurs. 
+                  Les moyens financiers limités de ces ménages les rendent aussi peu attractifs.
+                </p>
+          </div>
+      
+
+
+    </div>
+
+
+
+    </div>
+
+
+    
+
+   
+  
+ 
+   
+
+  </body>
+</html>
+
+
